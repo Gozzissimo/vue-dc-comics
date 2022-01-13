20d894e52c33bcc84543ecdb5ae84b21425408b1
Gozzissimo <fantini.nico@gmail.com>
2022-01-14 00:58:42 +0100

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,42 @@
 <template>
     <header>
-        PROVA
+        <div class="container flex">
+            <div class="logo">
+                <img src="../assets/img/dc-logo.png" alt="dc-logo">
+            </div>
+            <ul class="flex">
+                <li>
+                    <a href="#">characters</a>
+                </li>
+                <li>
+                    <a href="#">comics</a>
+                </li>
+                <li>
+                    <a href="#">movies</a>
+                </li>
+                <li>
+                    <a href="#">tv</a>
+                </li>
+                <li>
+                    <a href="#">games</a>
+                </li>
+                <li>
+                    <a href="#">collectibles</a>
+                </li>
+                <li>
+                    <a href="#">videos</a>
+                </li>
+                <li>
+                    <a href="#">fans</a>
+                </li>
+                <li>
+                    <a href="#">news</a>
+                </li>
+                <li>
+                    <a href="#">shop</a>
+                </li>
+            </ul>
+        </div>
     </header>
 </template>
 
@@ -16,5 +52,30 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+    @import "../assets/scss/partials/_variables.scss";
+    header {
+        margin: 20px 0;
+        color: $blackFontColor;
+        font-size: 1em;
+        .container {
+            justify-content: space-between;
+            .logo img {
+                width: 70%;
+            }      
+            ul {
+                align-items: center;
+                li a {
+                    display: block;
+                    padding: 1em;
+                    text-transform: uppercase;
+                    &:hover,
+                    &:focus,
+                    &:active {
+                        color: $blueFontColor;
+                        border-bottom: 2px solid $blueFontColor;
+                    }
+                }
+            }
+        }
+    }
 </style>
