8d23ecc6510d53459bb6e3156c08e98572664871
Gozzissimo <fantini.nico@gmail.com>
2022-01-13 18:18:48 +0100

    setup scss

--- src/App.vue
+++ src/App.vue
@@ -22,12 +22,5 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'PT Sans Narrow', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "./assets/scss/style.scss";
 </style>
