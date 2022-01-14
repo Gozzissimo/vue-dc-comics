2d91e5dc566912ad12e3c3b11fba7ab5224e7c5a
Gozzissimo <fantini.nico@gmail.com>
2022-01-14 17:17:39 +0100

    Import comics from data

---
+++ src/components/ComicsElement.vue
@@ -0,0 +1,21 @@
+<template>
+    <li>
+        <div>
+            <a href="#">
+                <img :src="thumbElement" :alt="seriesName">
+                <h3>{{ seriesName }}</h3>
+            </a>
+        </div>
+    </li>
+</template>
+
+<script>
+export default {
+    name: 'ComicsElement',
+    props: ['thumbElement', 'seriesName']
+}
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
